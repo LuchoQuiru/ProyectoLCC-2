5fa020ffb564db0da8b8b3ddff1d7617a3858a90
Fabricio <39932090+Fabri31295@users.noreply.github.com>
2020-06-04 17:29:00 -0300

    Rotaciones
    Metodos de rotar fila a la derecha o a la izquierda andando.

--- Proyecto.pl
+++ Proyecto.pl
@@ -3,13 +3,9 @@
 
 
 % Dir  -> Especifica la direccion del desplazamiento. Izq, der para fila, arriba o abajo para columna.
-
 % Num  -> Especifica el numero de fila o columna a desplazar (del 1 al 5).
-
 % Cant -> Especifica la cantidad de lugares a desplazar (del 1 al 4).
-
 % Tablero -> Especifica el tablero sobre el cual se aplicará el desplazamiento.
-
 % EvolTablero -> Es una lista de tableros que representa la evolucion del tablero original
 %	EvolTablero incluirá algunos de (posiblemente todos) los siguientes tableros:
 %		1. Resultante de realizar el desplazamiento (Dir,Num,Cant) sobre Tablero.
@@ -21,3 +17,35 @@
 %		4. Resultante de rellenar los espacios vacios en Tablero3 con mu~necas de tamanio chico y colores elegidos aleatoriamente.
 
 desplazar(Dir, Num, Cant, Tablero, EvolTablero).
+
+
+
+
+
+% Rota a Fila Cant veces a la izquierda
+desplazar_izquierda(Cant,Fila,Rta):-
+	Cant > 0,
+	rotar_izquierda(Fila,R),
+	Cant2 is Cant - 1,
+	desplazar_izquierda(Cant2,R,Rta).
+desplazar_izquierda(0,Fila,Fila).
+	
+% Coloca el ultimo elemento de la lista al inicio de la misma
+rotar_izquierda([H|T], R):- 
+	append(T, [H], R).
+
+% Rota a Fila Cant veces a la derecha
+desplazar_derecha(Cant,Fila,Rta):-
+	Cant > 0,
+	ultimo(Fila,U,R),
+	insertar_inicio(U,R,R1),
+	Cant2 is Cant-1,
+	desplazar_derecha(Cant2,R1,Rta).
+desplazar_derecha(0,Fila,Fila).
+
+% Elimina el ultimo elemento de la lista y lo retorna
+ultimo([L],L,[]).
+ultimo([L|Ls],U,[L|R]):-
+	ultimo(Ls,U,R).
+
+insertar_inicio(X,L,[X|L]).
