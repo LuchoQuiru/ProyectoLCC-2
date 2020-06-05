70aebfa4b746e4d6efd8dcf60b6ecfb25ca34b38
Fabricio <39932090+Fabri31295@users.noreply.github.com>
2020-06-05 14:18:34 -0300

    -

--- Proyecto.pl
+++ Proyecto.pl
@@ -25,23 +25,23 @@ desplazar(Dir, Num, Cant, Tablero, EvolTablero).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Rotar columna
 desplazar_arriba(Num,Cant,Tablero,Rta):-
-	quitar_elementos_columna(Num,Tablero,Elems,Tablero2),
+	obtener__columna(Num,Tablero,Elems,Tablero2),
 	desplazar_izquierda(Cant,Elems,Elems2),
 	insertar_elementos_columna(Num,Tablero2,Elems2,Rta).
 
 desplazar_abajo(Num,Cant,Tablero,Rta):-
-	quitar_elementos_columna(Num,Tablero,Elems,Tablero2),
+	obtener__columna(Num,Tablero,Elems,Tablero2),
 	desplazar_derecha(Cant,Elems,Elems2),
 	insertar_elementos_columna(Num,Tablero2,Elems2,Rta).
 
 % Obtiene los elementos correspondientes a la columna de la posicion Pos
-quitar_elementos_columna(Pos,[L|Ls],Elems,Rta):-
+obtener_columna(Pos,[L|Ls],Elems,Rta):-
 	posicion_de_lista(Pos,L,E,Fila),
-	quitar_elementos_columna(Pos,Ls,E2,R),
+	obtener__columna(Pos,Ls,E2,R),
 	insertar_inicio(Fila,R,Rta),
 	insertar_inicio(E,E2,Elems).
 
-quitar_elementos_columna(Pos,[L],[E],[Fila]):-
+obtener__columna(Pos,[L],[E],[Fila]):-
 	posicion_de_lista(Pos,L,E,Fila).
 
 insertar_elementos_columna(Pos,[Col|Cols],[Elem|Elems],Tablero):-
